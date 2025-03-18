b7fafd7cc208d58d40939209e7330d17516b0008
Elizabeth Passos <48329605+Elizabeth37@users.noreply.github.com>
2025-03-18 17:10:23 -0300

    Atualizando meu arquivo README

---
+++ src/emailapp/Email.java
@@ -0,0 +1,42 @@
+/*Projeto 1
+Aplicativo de e-mail
+Cenário: Você é um especialista em administração de suporte de TI e é encarregado da tarefa de criar contas de e-mail para novas contratações.
+Seu aplicativo deve fazer o seguinte:
+ Gerar um e-mail com a seguinte sintaxe: firstname.lastname@department.company.com
+ Determinar o departamento (vendas, desenvolvimento, contabilidade), se nenhum, deixe em branco
+ Gerar uma String aleatória para uma senha
+ Ter métodos definidos para alterar a senha, definir a capacidade da caixa de correio e definir um endereço de e-mail alternativo
+ Ter métodos get para exibir o nome, o e-mail e a capacidade da caixa de correio
+
+Projeto 1
+Aplicativo de e-mail
+Cenário: Você é um especialista em administração de suporte de TI e é encarregado da tarefa de criar contas de e-mail para novas contratações.
+Seu aplicativo deve fazer o seguinte:
+ Gerar um e-mail com a seguinte sintaxe: firstname.lastname@department.company.com
+ Determinar o departamento (vendas, desenvolvimento, contabilidade), se nenhum, deixe em branco
+ Gerar uma String aleatória para uma senha
+ Ter métodos definidos para alterar a senha, definir a capacidade da caixa de correio e definir um endereço de e-mail alternativo
+ Ter métodos get para exibir o nome, o e-mail e a capacidade da caixa de correio*/
+package emailApp;
+
+public class Email {
+    
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail; 
+
+    //Constructor to receive the first name and last name
+
+    //Ask for the department
+
+    //Generate a random password
+
+    //Set the mailbox capacity
+
+    //Set the alternate email
+
+    //Change the password
+}
---
+++ src/emailapp/EmailApp.java
@@ -0,0 +1,34 @@
+package emailApp;
+
+import java.util.Scanner;
+
+
+public class EmailApp {
+
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+
+        String firstName = sc.nextLine();
+
+        System.out.println("Informe seu primeiro nome: " + firstName + " ");
+
+
+        
+        /*String lastName;
+        String password;
+        String department;
+        int mailboxCapacity;
+        String alternateEmail;*/
+        // Corpo do método
+        //Declaração de variável tipo, nome, atribuição.
+        
+
+
+
+
+        sc.close();
+
+    }
+    }
+
